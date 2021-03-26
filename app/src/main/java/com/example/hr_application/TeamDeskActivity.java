b4501f5cfa105b0317c32b979c2f357c372a8533
Vinay <abhishaw5339@gmail.com>
2021-03-26 12:29:30 +0530

    error resolved in team desk list

--- app/src/main/java/com/example/hr_application/TeamDeskActivity.java
+++ app/src/main/java/com/example/hr_application/TeamDeskActivity.java
@@ -26,10 +26,9 @@ import java.util.ArrayList;
 public class TeamDeskActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private DatabaseReference databaseReference;
-    private String user,teamKey;
+    private String teamKey;
     private ArrayList<employeesModel> teamDeskEmployees;
-    private Boolean b=false;
-    private TextView teamName,leaderName,meetingLink,meetingTime, leader,meet,meet1,member;
+    private TextView teamName,leaderName,meetingLink,meetingTime;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     @Override
@@ -39,16 +38,12 @@ public class TeamDeskActivity extends AppCompatActivity {
         teamDeskEmployees = new ArrayList<>();
         teamName = findViewById(R.id.teamName);
 
-        leader = findViewById(R.id.leader);
         leaderName = findViewById(R.id.leaderName);
 
-        meet = findViewById(R.id.meet);
         meetingLink = findViewById(R.id.meetingLink);
 
-        meet1 = findViewById(R.id.meet1);
         meetingTime = findViewById(R.id.meetingTime);
 
-        member = findViewById(R.id.members);
         teamKey = getIntent().getStringExtra("teamKey");
 
         mRecyclerView = findViewById(R.id.teamDeskRV);
@@ -58,80 +53,15 @@ public class TeamDeskActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Team Desk");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        user = FirebaseAuth.getInstance().getCurrentUser().getUid();
         databaseReference = FirebaseDatabase.getInstance().getReference().child("teams");
-//        fetchUser();
         fetchDetails(teamKey);
     }
-
-    private void fetchUser() {
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot snapshot1:snapshot.getChildren()){
-//                    Log.d("userFound", "onDataChange: under Snapshot1");
-                    for (DataSnapshot snapshot2:snapshot1.child("members").getChildren()){
-//                        Log.d("userFound", "onDataChange: under Snapshot2");
-                        String userId = snapshot2.child("userid").getValue(String.class);
-                        if (user.equals(userId)){
-                            Toast.makeText(TeamDeskActivity.this, "userFound", Toast.LENGTH_SHORT).show();
-//                            Log.d("userFound", "onDataChange: "+userId);
-                            b=true;
-                            break;
-                        }
-                    }
-                    if (b){
-                        String key1 = snapshot1.getKey();
-//                        key = key1;
-                        fetchDetails(key1);
-//                        Toast.makeText(TeamDeskActivity.this, "key1", Toast.LENGTH_SHORT).show();
-//                        Log.d("userFound", "onDataChange: key1 = "+key1);
-                        break;
-                    }
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        if (!b) {
-            leader.setVisibility(View.INVISIBLE);
-            leaderName.setVisibility(View.INVISIBLE);
-
-            meet.setVisibility(View.INVISIBLE);
-            meetingLink.setVisibility(View.INVISIBLE);
-
-            meet1.setVisibility(View.INVISIBLE);
-            meetingTime.setVisibility(View.INVISIBLE);
-
-            member.setVisibility(View.INVISIBLE);
-            mRecyclerView.setVisibility(View.INVISIBLE);
-
-            teamName.setText("No Team Found");
-        }
-    }
-
     private void fetchDetails(String key1) {
-        leader.setVisibility(View.VISIBLE);
-        leaderName.setVisibility(View.VISIBLE);
-
-        meet.setVisibility(View.VISIBLE);
-        meetingLink.setVisibility(View.VISIBLE);
-
-        meet1.setVisibility(View.VISIBLE);
-        meetingTime.setVisibility(View.VISIBLE);
-
-        member.setVisibility(View.VISIBLE);
-        mRecyclerView.setVisibility(View.VISIBLE);
         databaseReference = FirebaseDatabase.getInstance().getReference().child("teams").child(key1);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()){
-//                    Log.d("userFound", "onDataChange: under fetchDetails");
                     String tN,lN,mL,mT;
                     tN = snapshot.child("teamName").getValue(String.class);
                     lN = snapshot.child("teamLead").getValue(String.class);
@@ -145,11 +75,6 @@ public class TeamDeskActivity extends AppCompatActivity {
                         employeesModel model = snapshot1.getValue(employeesModel.class);
                         teamDeskEmployees.add(model);
                     }
-//                    if (teamDeskEmployees.size() ==0){
-//
-//                    }
-//                    Log.d("userFound", "onDataChange: "+teamDeskEmployees.size());
-//                    Toast.makeText(TeamDeskActivity.this, "member Size"+teamDeskEmployees.size(), Toast.LENGTH_SHORT).show();
                     mAdapter = new teamDeskAdapter(TeamDeskActivity.this, teamDeskEmployees);
                     mRecyclerView.setAdapter(mAdapter);
                     mAdapter.notifyDataSetChanged();
--- app/src/main/java/com/example/hr_application/TeamDeskListActivity.java
+++ app/src/main/java/com/example/hr_application/TeamDeskListActivity.java
@@ -28,14 +28,12 @@ public class TeamDeskListActivity extends AppCompatActivity implements TeamDeskL
     private String user;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
-    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("teams");
-    private ArrayList<String> keys;
+    private DatabaseReference databaseReference;
     private ArrayList<TeamDeskListModel> deskListModels;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_desk_list);
-        keys = new ArrayList<>();
         deskListModels = new ArrayList<>();
         toolbar = findViewById(R.id.toolbar_deskList);
         mRecyclerView = findViewById(R.id.teamList);
@@ -48,17 +46,17 @@ public class TeamDeskListActivity extends AppCompatActivity implements TeamDeskL
         fetchKeys();
     }
     private void fetchKeys() {
-        deskListModels.clear();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("teams");
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                deskListModels.clear();
                 for (DataSnapshot snapshot1:snapshot.getChildren()){
                     for (DataSnapshot snapshot2:snapshot1.child("members").getChildren()){
                         String userId = snapshot2.child("userid").getValue(String.class);
                         if (user.equals(userId)){
-                            String key1 = snapshot1.getKey();
-                            Log.d("userFound", "onDataChange: "+key1);
-                            showKeys(key1);
+                            TeamDeskListModel model = snapshot1.getValue(TeamDeskListModel.class);
+                            deskListModels.add(model);
                             break;
                         }
                     }
@@ -75,34 +73,34 @@ public class TeamDeskListActivity extends AppCompatActivity implements TeamDeskL
 
     }
 
-    private void showKeys(String path) {
-//        for (int i=0;i<keys.size();i++){
-//            Log.d("userFound", "onDataChange: "+ keys);
-//            String path=keys.get(i);
-        //        }
-            databaseReference.child(path).addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                    Log.d("userFound", "onDataChange: data entry");
-                    if (snapshot.exists()){
-                        TeamDeskListModel model = snapshot.getValue(TeamDeskListModel.class);
-//                        String teamName,teamKey;
-//                        teamName = snapshot.child("teamName").getValue(String.class);
-//                        teamKey = snapshot.child("key").getValue(String.class);
-//                        Log.d("userFound", "onDataChange: name "+teamName);
-//                        Log.d("userFound", "onDataChange: key "+teamKey);
-//                        TeamDeskListModel model = new TeamDeskListModel(teamName, teamKey);
-                        deskListModels.add(model);
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    Log.d("userFound", "onDataChange: "+error);
-                }
-            });
-    }
+//    private void showKeys(String path) {
+////        for (int i=0;i<keys.size();i++){
+////            Log.d("userFound", "onDataChange: "+ keys);
+////            String path=keys.get(i);
+//        //        }
+//            databaseReference.child(path).addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                    Log.d("userFound", "onDataChange: data entry");
+//                    if (snapshot.exists()){
+//                        TeamDeskListModel model = snapshot.getValue(TeamDeskListModel.class);
+////                        String teamName,teamKey;
+////                        teamName = snapshot.child("teamName").getValue(String.class);
+////                        teamKey = snapshot.child("key").getValue(String.class);
+////                        Log.d("userFound", "onDataChange: name "+teamName);
+////                        Log.d("userFound", "onDataChange: key "+teamKey);
+////                        TeamDeskListModel model = new TeamDeskListModel(teamName, teamKey);
+//                        deskListModels.add(model);
+//                    }
+//                }
+//
+//                @Override
+//                public void onCancelled(@NonNull DatabaseError error) {
+//                    Log.d("userFound", "onDataChange: "+error);
+//                }
+//            });
+//    }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -112,7 +110,6 @@ public class TeamDeskListActivity extends AppCompatActivity implements TeamDeskL
 
     @Override
     public void onItemClicked(TeamDeskListModel model) {
-        Toast.makeText(this, ""+model.getKey(), Toast.LENGTH_SHORT).show();
         Log.d("key", "onItemClicked: "+model.getKey());
         Intent i = new Intent(this, TeamDeskActivity.class);
         i.putExtra("teamKey", model.getKey());
