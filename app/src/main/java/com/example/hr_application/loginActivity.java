615c2f6106528ad5a6e806dc292e30875e46ae87
Vinay <abhishaw5339@gmail.com>
2021-04-03 20:13:54 +0530

    added forget password and bug fixed in user profile activity.

--- app/src/main/java/com/example/hr_application/loginActivity.java
+++ app/src/main/java/com/example/hr_application/loginActivity.java
@@ -1,23 +1,29 @@
 package com.example.hr_application;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class loginActivity extends AppCompatActivity {
-    private TextView username;
+    private TextView username,forgetPassword;
     private TextView password;
     private FirebaseAuth auth;
     Button signup,login;
@@ -29,6 +35,7 @@ public class loginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         username=findViewById(R.id.emailInput);
         password=findViewById(R.id.passwordInput);
+        forgetPassword=findViewById(R.id.forgetPassword);
 
         login = (Button) findViewById(R.id.login);
         signup= (Button)findViewById(R.id.signupcheck);
@@ -39,6 +46,12 @@ public class loginActivity extends AppCompatActivity {
                startActivity(new Intent(loginActivity.this,signupActivity.class));
            }
 
+        });
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forgot_password(v);
+            }
         });
         login.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -53,6 +66,66 @@ public class loginActivity extends AppCompatActivity {
             }
 
         });
+    }
+    private Boolean validateEmailDialogBox(EditText reset,String val) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.+[a-z]+";
+
+        if (val.isEmpty()) {
+            Toast.makeText(loginActivity.this, "Email not be empty",Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!val.matches(emailPattern)) {
+            Toast.makeText(loginActivity.this, "Invalid Email",Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            reset.setError(null);
+            return true;
+        }
+    }
+    private void forgot_password(View v) {
+
+        EditText resetmail = new EditText(v.getContext());
+        resetmail.setBackgroundColor(getResources().getColor(R.color.textfieldcolor));
+        resetmail.setHint("Email");
+        resetmail.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        resetmail.setTextColor(getResources().getColor(R.color.colorAssent));
+        resetmail.setHintTextColor(getResources().getColor(R.color.colorAssent));
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(v.getContext(),R.style.MyDialogTheme);
+        alertDialog.setTitle(Html.fromHtml("<font color='#A8A8A8'>Reset Password?</font>"));
+        alertDialog.setMessage(Html.fromHtml("<font color='#A8A8A8'>Enter Your Email</font>"));
+        alertDialog.setView(resetmail);
+
+        alertDialog.setPositiveButton(Html.fromHtml("<font color='#A8A8A8'>YES</font>"), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Extract the email and sent reset link
+                String mail = resetmail.getText().toString().trim();
+                if(validateEmailDialogBox(resetmail,mail)) {
+                    auth.sendPasswordResetEmail(mail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(loginActivity.this, "Reset Link Has been Sent.", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(loginActivity.this, "Error! Reset Link is Not Sent. " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else {
+                    Toast.makeText(loginActivity.this, "Not Sent",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        alertDialog.setNegativeButton(Html.fromHtml("<font color='#A8A8A8'>NO</font>"), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Back to the login screen
+            }
+        });
+
+        alertDialog.create().show();
+
+
     }
 
     private void loginUser(String username,String password,String emailId) {
